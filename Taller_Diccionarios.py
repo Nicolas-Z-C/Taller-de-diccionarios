aefa53655945d9693796fc897fbea43c0b352fba
Nicolas-Z-C <nicolascampus117@gmail.com>
2025-08-12 20:33:18 -0500

    Start of the dictionaries and basic functions in blank alongside the space for the logic of the menus

---
+++ Taller_Diccionarios.py
@@ -0,0 +1,38 @@
+"""
+Autor:Nicolas Zabala Castaneda
+Fecha: 8/12/2025
+Descripcion: 
+"""
+#Importacion de modulos y librerias nesesarias 
+import  os 
+import random 
+
+#Iniciacion de las variables y diccionarios globales 
+
+#Debe contener dos subdiccionarios, uno de contacto y otro de direcciones 
+usuarioDatos = {}
+productosDisp = {}
+#Es un subdiccionario por cada uno de los productos
+productosCliente = {}
+#Historial de los movimientos de las cuentas 
+histMov = {}
+
+#Definicion de las funciones que se van a usar junto a los menus
+def NuevaCuenta():
+    pass 
+def Depositar():
+    pass
+def SolicitarProducto(): 
+    pass 
+def Pagodesaldo():
+    pass
+def Saldopendiente():
+    pass
+def EliminarCuenta():
+    pass 
+def MenuInicio():
+    pass
+def MenuProductos():
+    pass
+
+#Maquetacion de los menus que se van a formar
